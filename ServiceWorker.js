78298dff3e61e4a926f056627fb3808194b9e84a
Valdas Jankaitis <valdasjankaitis@gmail.com>
2025-08-27 22:25:24 +0800

    Base Build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Jam Creative Studios-0723_BreconMuseum-0.1.0";
+const contentToCache = [
+    "Build/0723_BreconMuseum.loader.js",
+    "Build/0723_BreconMuseum.framework.js.unityweb",
+    "Build/0723_BreconMuseum.data.unityweb",
+    "Build/0723_BreconMuseum.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
